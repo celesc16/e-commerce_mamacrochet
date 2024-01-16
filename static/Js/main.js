0f9cda0fc76018fe4d3d78f8bcc782194282c307
celechoquevillca2004gmail.com <celechoquevillca2004@gmail.com>
2024-01-15 21:22:23 -0300

    add:mainjs

---
+++ static/Js/main.js
@@ -0,0 +1,87 @@
+let products = []
+
+//Todos los productos 
+fetch("/Json/products.json")
+    .then(response => response.json())
+    .then(data => {
+        products = data
+        carryProducts(products)
+    })
+
+const containerProducts = document.querySelector("#container-products-popular");
+const containerProductsOfertas = document.querySelector("#container-products-ofertas");
+let buttonTrolley = document.querySelectorAll(".product-trolley");
+const number = document.querySelector("#number");
+
+
+function carryProducts(){
+    
+    products.forEach(product => {
+        const div = document.createElement("div");
+        div.classList.add("product")
+        div.innerHTML = `
+            <img class="product-img" src="${product.imagen}" alt="${product.titulo}" >
+            <div class="product-details">
+                <h3 class="product-title">${product.titulo}</h3>
+                <p class="product-price" >${product.precio}</p>
+                <button class="product-trolley" id="${product.id}" >Agregar al Carrito</button>
+            </div>
+        `;
+
+        if(product.clase === "MasPopulares"){
+            containerProducts.
+            appendChild(div);
+        }
+        else if(product.clase === "Ofertas"){
+            containerProductsOfertas.
+            appendChild(div);
+        }
+
+        //containerProducts.append(div)
+
+    
+    })
+
+    updateButtonAdd();
+}
+
+carryProducts(products);
+
+//Las categorias 
+
+//carritoagregar
+
+function updateButtonAdd(){
+    buttonTrolley = document.querySelectorAll(".product-trolley");
+
+    buttonTrolley.forEach(button => {
+        button.addEventListener("click" , addtrolley)
+    });
+}
+
+const productsInTrolley = []
+
+function addtrolley(e){
+    const idBottom = e.currentTarget.id
+    const productAdd = products.find(product => product.id === idBottom)
+
+    if (productsInTrolley.some( product => product.id ===idBottom)){
+        const index = productsInTrolley.findIndex(product => product.id === idBottom)
+        productsInTrolley[index].cantidad++;
+
+    } else{
+        productAdd.cantidad = 1 
+        productsInTrolley.push(productAdd) 
+    }
+
+    updateNumber()
+
+    localStorage.setItem("product-in-trolley", JSON.stringify(productsInTrolley))
+}
+
+function updateNumber(){
+    let newNumber = productsInTrolley.reduce((acc,product) => acc + product.cantidad, 0);
+    number.innerText = newNumber
+}
+
+
